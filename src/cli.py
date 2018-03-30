06f49df07d764495e5d9ff5dbf144bcc572dfc23
Hiruna Jayamanne <hirunav2@gmail.com>
2018-03-31 00:14:53 +1300

    Reload flag not yet usable.

--- src/cli.py
+++ src/cli.py
@@ -3,6 +3,5 @@ import argparse
 def parse():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--info", action="store_true", help="Sets the logging module to INFO")
-    parser.add_argument("-r", "--reload", action="store_true", help="Restarts the bot after shutting down due to an error or otherwise")
     parser.add_argument("-t", "--token", help="Sets the token for the bot")
     return parser.parse_args()
